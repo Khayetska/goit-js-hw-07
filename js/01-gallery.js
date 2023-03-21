36d955daf3455772be7eb5dc1727fc944ac198b0
Khayetska <113913429+Khayetska@users.noreply.github.com>
2023-03-22 00:02:00 +0200

    Did first task about gallery

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,56 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const galleryListEl = document.querySelector(".gallery");
 
-console.log(galleryItems);
+const galleryItemsMarkup = createGalleryItemsMarkup(galleryItems);
+
+galleryListEl.insertAdjacentHTML("afterbegin", galleryItemsMarkup);
+
+function createGalleryItemsMarkup(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return `
+    <li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+            />
+        </a>
+    </li>`;
+    })
+    .join("");
+}
+
+galleryListEl.addEventListener("click", handleClickOnGalleryItem);
+
+function handleClickOnGalleryItem(event) {
+  event.preventDefault();
+
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+  const instance = basicLightbox.create(
+    `
+    <img src="${event.target.dataset.source}">
+`,
+    {
+      onShow: (instance) => {
+        document.addEventListener("keydown", escapeImg);
+      },
+      onClose: (instance) => {
+        document.removeEventListener("keydown", escapeImg);
+      },
+    }
+  );
+
+  instance.show();
+
+  function escapeImg({ code }) {
+    if (basicLightbox.visible() === true && code === "Escape") {
+      instance.close();
+    }
+  }
+}
